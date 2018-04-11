e4411ab25dbaf3290882b2dc49467301f5201273
adam n <adam.nawroc@gmail.com>
2018-04-11 13:51:43 +0000

    progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,6 +13,10 @@ class SongsController < ApplicationController
     
   end
 
+  def create
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_song
