9d4efcb5faf23136713db6986179a64dc76448d1
adam n <adam.nawroc@gmail.com>
2018-04-11 13:50:58 +0000

    progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,26 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_song
+      @song = Song.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # def article_params
+    #   params.require(:article).permit(:title, :body)
+    # end
 end
