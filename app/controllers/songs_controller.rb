7771a48f6a51dd198b12924e9cc0122e6dc92f82
adam n <adam.nawroc@gmail.com>
2018-04-12 11:19:57 +0000

    progress

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -40,13 +40,18 @@ class SongsController < ApplicationController
       # @post.save
       # This returns a status_code of 302, which instructs the browser to
       # worry about re-reading it from the database)
-      redirect_to song_path(@post)
+      redirect_to song_path(@song)
     else
       # If the post is invalid, hold on to @post, because it is now full of
       # useful error messages, and re-render the :new page, which knows how
       # to display them alongside the user's entries.
       render :edit
     end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to songs_url
   end
 
   private
