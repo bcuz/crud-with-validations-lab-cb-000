fcf5f30df344cf0c05146c20392d671fc9cfa698
adam n <adam.nawroc@gmail.com>
2018-04-12 12:31:54 +0000

    progress

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
-  # validates :release_year, presence: true unless :released == false
+  # somehow
+  validates :title, presence: true, :uniqueness => { :scope => :release_year }
   validates :release_year, presence: true, :if => :should_have_year?, numericality: { less_than_or_equal_to: Time.now.year }
-  # validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
-  # attr_accessor :released
+
+  # uniq title based on release_year ?
 
   # works, not sure if its the best way.
   # https://stackoverflow.com/questions/13750436/rails-validating-a-field-is-present-only-if-another-is-present
