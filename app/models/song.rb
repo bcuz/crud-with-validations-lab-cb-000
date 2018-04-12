654c391adb0d4b507880333de8ff40fb0e8e3341
adam n <adam.nawroc@gmail.com>
2018-04-12 12:16:54 +0000

    progress

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,13 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  # validates :release_year, presence: true unless :released == false
+  validates :release_year, presence: true, :if => :should_have_year?, numericality: { less_than_or_equal_to: Time.now.year }
+  # validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+  # attr_accessor :released
 
+  # works, not sure if its the best way.
+  # https://stackoverflow.com/questions/13750436/rails-validating-a-field-is-present-only-if-another-is-present
+  def should_have_year?
+    self.released
+  end
 end
